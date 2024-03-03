fe69cd44221271896fa458d81751c87f48e753e5
MichealRonan <132925162+MichealRonan@users.noreply.github.com>
2024-03-03 11:08:48 -0600

    InitialCommit

--- Desktop/live-map-tracking-frontend/src/MapComponent.js
+++ Desktop/live-map-tracking-frontend/src/MapComponent.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import io from 'socket.io-client';
+import './MapComponent.css';
 
 const MapComponent = () => {
     const [position, setPosition] = useState([39.1653, -86.5264]);
+    const [trackingInfo, setTrackingInfo] = useState('Mark');
 
     useEffect(() => {
         const socket = io('http://localhost:4000');
@@ -17,17 +19,25 @@ const MapComponent = () => {
     }, []);
 
     return (
-        <MapContainer center={position} zoom={13} style={{ height: '400px', width: '50%' }}>
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            />
-            <Marker position={position}>
-                <Popup>
-                    Current Position<br />Easily customizable.
-                </Popup>
-            </Marker>
-        </MapContainer>
+        <div className="map-page">
+            <header className="map-header">
+                <h1>DeliverEase</h1>
+            </header>
+            <MapContainer center={position} zoom={13} style={{ height: '400px', width: '80%' }}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker position={position}>
+                    <Popup>
+                        Current Position<br />Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+            <div className="tracking-info">
+                Tracking: {trackingInfo}
+            </div>
+        </div>
     );
 };
 
